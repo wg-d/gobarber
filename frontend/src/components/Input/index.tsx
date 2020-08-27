e816bcad9c7f0755b0697b18492b1c51c85c8af1
wg-d <wonderdream.dev@outlook.com>
2020-08-27 01:22:24 -0300

    Validation Errors

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -41,6 +41,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         onFocus={handleInputFocus}
         onBlur={handleInputBlur}
       />
+      {error}
     </Container>
   );
 };
--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,16 +1,22 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useRef} from 'react';
 import { Container, Content, Background } from './styles';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
+import getValidationErrors from '../../utils/getValidationErrors';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input/index';
 import Button from '../../components/Button/index';
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: Object) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
@@ -23,8 +29,11 @@ const SignIn: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false
       });
-    } catch (error) {
-      console.log(error);
+
+    } catch (err) {
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -34,7 +43,7 @@ const SignIn: React.FC = () => {
       <Content>
         <img src={logoImg} alt="logo" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input icon={FiUser} name="name" placeholder="Nome" />
           <Input icon={FiMail} name="email" placeholder="E-mail" />
