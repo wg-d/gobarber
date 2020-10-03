8bab822b462a80b17ffb3e2ea9a9faa3a1f11ace
wg-d <wonderdream.dev@outlook.com>
2020-10-03 18:52:53 -0300

    Controllers

--- backend/src/modules/appointments/infra/http/routes/appointments.route.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.route.ts
@@ -1,31 +1,12 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import {container} from 'tsyringe';
-
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 import EnsureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsController();
 
 appointmentsRouter.use(EnsureAuthenticated);
 
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-//   const appointments = await appointmentsRepository.find();
-//   return response.json(appointments);
-// });
-
-appointmentsRouter.post('/', async (request, response) => {
-  const { provider_id, date } = request.body;
-  const parsedDate = parseISO(date);
-
-  const createAppointment = container.resolve(CreateAppointmentService);
-
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-  return response.json(appointment);
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
--- backend/src/modules/users/infra/http/routes/sessions.route.ts
+++ backend/src/modules/users/infra/http/routes/sessions.route.ts
@@ -1,21 +1,9 @@
 import { Router } from 'express';
-import {container} from 'tsyringe';
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import SessionsController from '../controllers/SessionsController';
 
 const sessionsRouter = Router();
+const sessionsController = new SessionsController();
 
-sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
-
-  const authenticateUserService = container.resolve(AuthenticateUserService);
-
-  const { user, token } = await authenticateUserService.execute({
-    email,
-    password,
-  });
-
-  delete user.password;
-  return response.json({ user, token });
-});
+sessionsRouter.post('/', sessionsController.create);
 
 export default sessionsRouter;
